3f641e5571a00254986d8d4d8ed499a253563244
Cesar Roman <cesardroman18@gmail.com>
2023-09-28 16:13:03 -0500

    able to render out custom object data to client from API call

--- index.js
+++ index.js
@@ -12,15 +12,35 @@ const PRIVATE_APP_ACCESS = "pat-na1-61a47881-66e7-4fa2-88a6-5dacbc51c360";
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+  const customObjs = "https://api.hubspot.com/crm/v3/objects/players";
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  };
+
+  try {
+    const response = await axios.get(customObjs, { headers });
+    const data = response.data.results;
+    res.json(data)
+  } catch (error) {
+    console.error(error) 
+  }
+
+})
+
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {
+
+})
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+
+})
 
 /**
 * * This is sample code to give you a reference for how you should structure your calls.
